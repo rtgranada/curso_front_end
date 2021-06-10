854878e25ffc53cec141a91ebd8c58786fc5877c
rodrigo <rtgranada@gmail.com>
2021-06-10 15:18:46 -0300

    Exercicios aula 1 de JS

---
+++ ex_aula_01.js
@@ -0,0 +1,15 @@
+let primeiroNumero = 42
+let segundoNumero = -3.14
+console.log('Soma: ' + primeiroNumero * segundoNumero)
+
+const name = 'Otter' + 'wise'
+console.log('Name: ' + name)
+
+let operationAnd = true && false
+console.log('And: ' + operationAnd)
+
+let operationOr = true || false
+console.log('Or: ' + operationOr)
+
+let operationNot = !false
+console.log('Not: ' + operationNot)
