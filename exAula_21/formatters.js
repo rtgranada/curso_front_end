26707c1ba08ac72e34f43eaedd0812c637dc82cb
rodrigo <rtgranada@gmail.com>
2021-07-07 16:00:58 -0300

    modularicazao Aula 21

---
+++ exAula_21/formatters.js
@@ -0,0 +1,8 @@
+import { format } from 'date-fns'
+import { parseISO } from 'date-fns'
+
+export function formatDate(data) {
+  console.log(data)
+  // console.log(parseISO(data))
+  //console.log(format(parseISO(data), 'dd/MM/yyyy'))
+}
---
+++ exAula_21/index.js
@@ -0,0 +1,3 @@
+import { formatDate } from './formatters.js'
+
+formatDate(new Date('20/04/2021'))
